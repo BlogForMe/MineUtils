25b9dc12e3db69b4158f98cf8fb3cd34605dd802
zhouheng <zhouheng@tngdigital.com>
2022-07-26 10:43:51 +0800

    feat: new plugin

---
+++ plugin/src/main/java/com/great/plugin/JiaGuExt.kt
@@ -0,0 +1,17 @@
+package com.great.plugin
+
+/**
+ *
+ * ClassName:      JiaGuExt
+ * Description:    Description
+ * Author:         zh
+ * CreateDate:     2022/7/26 7:56 AM
+ * UpdateUser:     zh
+ * UpdateDate:     2022/7/26 7:56 AM
+ * UpdateRemark:   Modify the description
+ */
+
+class JiaGuExt(
+    val userName: String,
+    val password: String
+)
--- plugin/src/main/java/com/great/plugin/MyClass.kt
+++ plugin/src/main/java/com/great/plugin/MyClass.kt
@@ -1,3 +1,28 @@
 package com.great.plugin
 
-class MyClass
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.api.ApplicationVariant
+import org.gradle.api.Action
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+//https://www.bilibili.com/video/BV1dp4y1e7W4?p=4
+class JiaGuPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        val jiagu = project.extensions.create("jiagu", JiaGuExt::class.java)
+
+        project.afterEvaluate(object : Action<Project> {
+            override fun execute(project: Project) {
+                project.extensions.getByName("android")
+                //能够动态的获取当前的(debug/release)的apk文件
+                val android = project.extensions.getByType(AppExtension::class.java)
+                android.applicationVariants.all(object : Action<ApplicationVariant> {
+                    override fun execute(applicationVariant: ApplicationVariant) {
+                        val name = applicationVariant.name
+                        applicationVariant.outputs
+                    }
+                })
+            }
+        })
+    }
+}
