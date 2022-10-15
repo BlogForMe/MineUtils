47beb9d27dd5a17c58bc3f516481a3a386b5c2fd
zhouheng <zhouheng@tngdigital.com>
2022-10-15 20:22:49 +0800

    feat: dagger java to  kotlin

--- app/src/main/java/com/comm/util/dagger/dn/DaggerActivity.kt
+++ app/src/main/java/com/comm/util/dagger/dn/DaggerActivity.kt
@@ -1,48 +1,45 @@
-package com.comm.util.dagger.dn;
+package com.comm.util.dagger.dn
 
-import javax.inject.Inject;
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.comm.util.R
+import com.comm.util.dagger.dn.di.ApiService
+import com.comm.util.dagger.dn.di.DaggerApplicationComponent
+import com.comm.util.dagger.dn.di.User
+import retrofit2.Retrofit
+import javax.inject.Inject
 
-import android.os.Bundle;
-import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
-import com.comm.util.R;
-import com.comm.util.dagger.dn.di.ApiService;
-import com.comm.util.dagger.dn.di.DaggerApplicationComponent;
-import com.comm.util.dagger.dn.di.User;
-import retrofit2.Retrofit;
-
-public class DaggerActivity extends AppCompatActivity {
-    String TAG = "DaggerActivity";
+class DaggerActivity : AppCompatActivity() {
+    var TAG = "DaggerActivity"
 
     //3.设置Inject注解
-    @Inject
-    User user;
 
     @Inject
-    User user2;
+    lateinit var user: User
 
     @Inject
-    Retrofit retrofit;
+    lateinit var user2: User
 
     @Inject
-    ApiService apiService;
+    lateinit var retrofit: Retrofit
+
+    @Inject
+    lateinit var apiService: ApiService
 
     //@Inject
     //ApiService apiService2;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dagger2);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dagger2)
         //4.执行注入动作
-        DaggerApplicationComponent.create().inject(this);//DaggerApplicationComponent相当于DaggerApplication的实现类
+        DaggerApplicationComponent.create()
+            .inject(this) //DaggerApplicationComponent相当于DaggerApplication的实现类
 
-        Log.i(TAG, "user: " + user);
-        Log.i(TAG, "user2: " + user2);
-
-        Log.i(TAG, "retrofit: " + retrofit);
-
-        Log.i(TAG, "apiService: " +apiService);
+        Log.i(TAG, "user: $user")
+        Log.i(TAG, "user2: $user2")
+        Log.i(TAG, "retrofit: $retrofit")
+        Log.i(TAG, "apiService: $apiService")
         //Log.i(TAG, "apiService2: " +apiService2);
 
         //startActivity(new Intent(this,SecondActivity.class));
--- app/src/main/java/com/comm/util/dagger/dn/DaggerActivity1.kt
+++ app/src/main/java/com/comm/util/dagger/dn/DaggerActivity1.kt
@@ -1,52 +1,49 @@
-package com.comm.util.dagger.dn;
+package com.comm.util.dagger.dn
 
-import javax.inject.Inject;
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.comm.util.MyApplication
+import com.comm.util.R
+import com.comm.util.dagger.dn.di.ApiService
+import com.comm.util.dagger.dn.di.User
+import retrofit2.Retrofit
+import javax.inject.Inject
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
-import com.comm.util.MyApplication;
-import com.comm.util.R;
-import com.comm.util.dagger.dn.di.ApiService;
-import com.comm.util.dagger.dn.di.User;
-import retrofit2.Retrofit;
-
-public class DaggerActivity1 extends AppCompatActivity {
-    String TAG = "DaggerActivity1";
+class DaggerActivity1 : AppCompatActivity() {
+    var TAG = "DaggerActivity1"
 
     //3.设置Inject注解
-    @Inject
-    User user;
 
     @Inject
-    User user2;
+    lateinit var user: User
+
 
     @Inject
-    Retrofit retrofit;
+    lateinit var user2: User
+
 
     @Inject
-    ApiService apiService;
+    lateinit var retrofit: Retrofit
+
 
     @Inject
-    ApiService apiService2;
+    lateinit var apiService: ApiService
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dagger2);
+    @Inject
+    lateinit var apiService2: ApiService
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dagger2)
         //4.执行注入动作
         //DaggerApplicationComponent1.create().inject(this);//DaggerApplicationComponent相当于DaggerApplication的实现类
-        MyApplication.getApplicationComponent1().inject(this);
-
-        Log.i(TAG, "user: " + user);
-        Log.i(TAG, "user2: " + user2);
-
-        Log.i(TAG, "retrofit: " + retrofit);
-
-        Log.i(TAG, "apiService: " +apiService);
-        Log.i(TAG, "apiService2: " +apiService2);
-
-        startActivity(new Intent(this, DaggerSecondActivity.class));
+        MyApplication.getApplicationComponent1().inject(this)
+        Log.i(TAG, "user: $user")
+        Log.i(TAG, "user2: $user2")
+        Log.i(TAG, "retrofit: $retrofit")
+        Log.i(TAG, "apiService: $apiService")
+        Log.i(TAG, "apiService2: $apiService2")
+        startActivity(Intent(this, DaggerSecondActivity::class.java))
     }
 }
